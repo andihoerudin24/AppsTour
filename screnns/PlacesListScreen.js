51e2babc880fc643bb5a7de1dee80a2575fcfed1
Andi Hoerduin <andihoerudin24@gmail.com>
2020-05-10 04:10:36 +0700

    load data

--- screnns/PlacesListScreen.js
+++ screnns/PlacesListScreen.js
@@ -1,12 +1,19 @@
-import React from "react";
-import { View, Text, StyleSheet, Platform, FlatList } from "react-native";
+import React,{useEffect} from "react";
+import { View, Text, StyleSheet, Platform, FlatList} from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
 import PlaceitemComponent from "../components/placeitem";
+import * as placesAction from '../store/places-actions'
 
 const PlacesListScreen = (props) => {
   const place = useSelector((state) => state.places.places);
+  const dispacth = useDispatch()
+
+  useEffect(()=>{
+    dispacth(placesAction.loadPlaces());
+  },[dispacth])
+
   return (
     <FlatList
       data={place}
--- store/places-actions.js
+++ store/places-actions.js
@@ -1,21 +1,44 @@
-import { Asset, Constants, FileSystem, Permissions } from 'react-native-unimodules';
+import * as FileSystem from 'expo-file-system';
+
+import { insertPlace, fetchPlaces } from '../helpers/db';
+
 export const ADD_PLACE = 'ADD_PLACE';
+export const SET_PLACES = 'SET_PLACES';
+
 export const addPlace = (title, image) => {
   return async dispatch => {
-    console.log(image)
     const fileName = image.split('/').pop();
-    console.log('file',fileName)
-    const newPath =  FileSystem.documentDirectory + fileName;
-    console.log('new',newPath);
+    const newPath = FileSystem.documentDirectory + fileName;
+
     try {
       await FileSystem.moveAsync({
         from: image,
         to: newPath
       });
+      const dbResult = await insertPlace(
+        title,
+        newPath,
+        'Dummy address',
+        15.6,
+        12.3
+      );
+      console.log(dbResult);
+      dispatch({ type: ADD_PLACE, placeData: { id: dbResult.insertId, title: title, image: newPath } });
     } catch (err) {
       console.log(err);
       throw err;
     }
-    dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
   };
+};
+
+export const loadPlaces = () => {
+    return async dispatch => {
+        try {
+            const dbResult = await fetchPlaces();
+            console.log(dbResult);
+            dispatch({ type: SET_PLACES, places: dbResult.rows._array });
+        } catch (err) {
+            throw err;
+        }
+    };
 };
